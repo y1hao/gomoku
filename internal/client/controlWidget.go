042c9d1e2c532a199d92c768e4d400032897ad78
Yihao <48015192+CoderYihaoWang@users.noreply.github.com>
2021-02-21 01:22:19 +1300

    Setup score widget

--- internal/client/controlWidget.go
+++ internal/client/controlWidget.go
@@ -17,3 +17,38 @@ func NewControlWidget(row, col, height, width int) *ControlWidget {
 		},
 	}
 }
+
+func (w *ControlWidget) Draw() {
+	w.BoxWidget.Draw()
+
+	pushPosition()
+	defer popPosition()
+
+	setPosition(w.row+1, w.col+1)
+	print(highlightF, blackB, "Exit")
+	print(infoF, blackB, ": ")
+	print(greenF, blackB, "exit")
+
+	setPosition(w.row+2, w.col+1)
+	print(highlightF, blackB, "Move")
+	print(infoF, blackB, ": <")
+	print(highlightF, blackB, "c")
+	print(infoF, blackB, "><")
+	print(highlightF, blackB, "r")
+	print(infoF, blackB, ">")
+
+	setPosition(w.row+3, w.col+3)
+	print(infoF, blackB, "eg: ")
+	print(greenF, blackB, "h8")
+
+	setPosition(w.row+4, w.col+1)
+	print(highlightF, blackB, "Chat")
+	print(infoF, blackB, ": ")
+	print(greenF, blackB, "'")
+	print(highlightF, blackB, "...")
+	print(greenF, blackB, "'")
+
+	setPosition(w.row+5, w.col+3)
+	print(infoF, blackB, "eg: ")
+	print(greenF, blackB, "'Yo!'")
+}
--- internal/client/scoreWidget.go
+++ internal/client/scoreWidget.go
@@ -1,5 +1,7 @@
 package client
 
+import "fmt"
+
 type ScoreWidget struct {
 	BoxWidget
 	context *Context
@@ -20,6 +22,25 @@ func NewScoreWidget(row, col, height, width int, context *Context) *ScoreWidget 
 	}
 }
 
-func (w *ScoreWidget) Redraw() {
+func (w *ScoreWidget) Draw() {
+	w.BoxWidget.Draw()
 
+	pushPosition()
+	defer popPosition()
+
+	setPosition(w.row+1, w.col+2)
+	print(infoF, blackB, "You:")
+	print(youF, blackB, fmt.Sprintf("% 6d", w.context.Score1))
+
+	setPosition(w.row+2, w.col+2)
+	print(infoF, blackB, "Opp:")
+	print(oppF, blackB, fmt.Sprintf("% 6d", w.context.Score2))
+}
+
+func (w *ScoreWidget) Redraw() {
+	setPosition(w.row+1, w.col+2+len("You:"))
+	print(youF, blackB, fmt.Sprintf("% 6d", w.context.Score1))
+
+	setPosition(w.row+2, w.col+2+len("Opp:"))
+	print(oppF, blackB, fmt.Sprintf("% 6d", w.context.Score2))
 }
